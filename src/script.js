a6315b5db1de5251f53a989ea9e7b3fa574a6b8c
pankiale <93738391+pankiale@users.noreply.github.com>
2022-02-15 15:45:43 +0100

    fy

--- src/script.js
+++ src/script.js
@@ -20,12 +20,13 @@ accordionButtons.forEach((accordionButton) => {
 	});
 });
 // ------------------------------ execution ---------------------------- //
-const  birthDate = document.querySelector('.birthday');
+const  birthDate = document.querySelector('#birthday');
+console.log(birthDate);
 const dateMask = IMask(birthDate, {
 	mask: Date,  // enable date mask
 
 	// other options are optional
-	pattern: 'Y-`m-`d',  // Pattern mask with defined blocks, default is 'd{.}`m{.}`Y'
+	pattern: 'd-`m-`Y',  // Pattern mask with defined blocks, default is 'd{.}`m{.}`Y'
 	// you can provide your own blocks definitions, default blocks for date mask are:
 	blocks: {
 	  d: {
@@ -42,36 +43,8 @@ const dateMask = IMask(birthDate, {
 	  },
 	  Y: {
 		mask: IMask.MaskedRange,
-		from: 1900,
-		to: 9999,
+		from: 1920,
+		to: 2010,
 	  }
 	},
-	// define date -> str convertion
-	format: function (date) {
-	  var day = date.getDate();
-	  var month = date.getMonth() + 1;
-	  var year = date.getFullYear();
-
-	  if (day < 10) day = "0" + day;
-	  if (month < 10) month = "0" + month;
-
-	  return [year, month, day].join('-');
-	},
-	// define str -> date convertion
-	parse: function (str) {
-	  var yearMonthDay = str.split('-');
-	  return new Date(yearMonthDay[0], yearMonthDay[1] - 1, yearMonthDay[2]);
-	},
-
-	// optional interval options
-	min: new Date(2000, 0, 1),  // defaults to `1900-01-01`
-	max: new Date(2020, 0, 1),  // defaults to `9999-01-01`
-
-	autofix: true,  // defaults to `false`, see details
-
-	// also Pattern options can be set
-	lazy: false,
-
-	// and other common options
-	overwrite: true  // defaults to `false`
   });
