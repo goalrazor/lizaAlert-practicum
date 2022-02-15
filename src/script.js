f412ae806fc926f67544459fd87cf90f1609dcc3
vartuom <v.artuom@ya.ru>
2022-02-15 19:51:44 +0800

    linter fix 2

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 // ------------------------------ var\const ---------------------------- //
 const content = document.querySelector('.page');
 const accordionButtons = content.querySelectorAll('.accordion__button');
-
 const elChipTemplate = content.querySelector('#chip-template').content;
 const chipsList = document.querySelector('.tags-list');
 const checkboxLists = document.querySelectorAll('.filter__checkbox-list');
 let checkedBoxes = {};
-
 // ------------------------------ functions ---------------------------- //
 function toggleAccordion(el) {
     const elChevron = el.querySelector('.accordion__chevron');
@@ -19,14 +17,12 @@ function toggleAccordion(el) {
         elDropPanel.style.maxHeight = elDropPanel.scrollHeight + 'px';
     }
 }
-
 function parseCheckboxData(input) {
     return {
         label: input.parentNode.querySelector('.filter__label-text').textContent.trim(),
         modifier: input.getAttribute('name'),
     };
 }
-
 function addChip(input) {
     const checkboxData = parseCheckboxData(input);
     const checkboxLabel = checkboxData.label;
@@ -35,7 +31,6 @@ function addChip(input) {
     elChip.classList.add('filter-chip_ctrl_' + checkboxModifier.substring(9)); // отпиливаем от имени чекбокса 'checkbox-'
     elChip.querySelector('.filter-chip__text').textContent = checkboxLabel;
     checkedBoxes[checkboxLabel] = { input, chip: elChip };
-
     function handleClickChips(e) {
         e.preventDefault();
         checkedBoxes[checkboxLabel].input.checked = false;
@@ -43,17 +38,14 @@ function addChip(input) {
         elChip.remove();
     }
     elChip.addEventListener('click', handleClickChips);
-
     return elChip;
 }
-
 function removeChip(input) {
     const checkboxLabel = parseCheckboxData(input).label;
     const elChip = checkedBoxes[checkboxLabel].chip;
     delete checkedBoxes[checkboxLabel];
     elChip.remove();
 }
-
 function handleInputCheckbox(e) {
     const inputCheckbox = e.target;
     if (inputCheckbox.classList.contains('filter__checkbox')) {
@@ -64,14 +56,12 @@ function handleInputCheckbox(e) {
         }
     }
 }
-
 // ------------------------------ listeners ---------------------------- //
 accordionButtons.forEach((accordionButton) => {
     accordionButton.addEventListener('click', function () {
         toggleAccordion(this);
     });
 });
-
 checkboxLists.forEach((checkboxList) => {
     checkboxList.addEventListener('input', handleInputCheckbox);
 });
